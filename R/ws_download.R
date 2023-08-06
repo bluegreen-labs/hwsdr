482c883af603d6d66c8bc3de4a60054415c852c8
Koen Hufkens <koen.hufkens@gmail.com>
2023-08-06 17:53:49 +0200

    download docs

--- R/ws_download.R
+++ R/ws_download.R
@@ -2,6 +2,16 @@
 #'
 #' Downloads both the database and gridded HWSD v2.0 data products
 #' to a desired output path for subsetting.
+#' 
+#' When an existing path is used which is not the temporary directory
+#' an environmental variable WS_PATH will be set. This variable will
+#' override the default temporary directory if it exists. This allows
+#' the gridded data to be stored elsewhere and be forgotten (while using the
+#' {hwsdr} package for HWSD v2.0).
+#' 
+#' Should you delete the gridded file, than it can be downloaded again but
+#' the environmental variable should be set again using the new_path = TRUE
+#' flag.
 #'
 #' @param ws_path the path / directory where to store the HWSD v2.0 database
 #' @param new_path update the path, i.e. dowload data to a new local
@@ -11,6 +21,27 @@
 #' @export
 #'
 #' @examples
+#' 
+#' \dontrun{
+#'  
+#'  # Download the gridded soil map of
+#'  # HWSD v2.0 to the temporary directory
+#'  ws_download()
+#'  
+#'  # download the same data to a specific
+#'  # directory (which should exist)
+#'  ws_download(
+#'  ws_path = "~/my_path"
+#'  )
+#'  
+#'  # download the same data to a specific
+#'  # directory (which should exist) and
+#'  # update the environmental variable
+#'  ws_download(
+#'  ws_path = "~/my_path",
+#'  new_path = TRUE
+#'  )
+#' }
 
 ws_download <- function(
     ws_path = file.path(tempdir(), "ws_db"),
